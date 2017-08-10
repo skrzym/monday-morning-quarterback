b7bac7dd2730b191f02aba6b6f3dbb2fe4898b4e
Michael Skrzypiec <skrzym@gmail.com>
2017-08-10 14:54:51 -0700

    include playoff data in model

--- Research/run_model.py
+++ Research/run_model.py
@@ -1,7 +1,9 @@
 import nfl_model
 from sklearn.externals import joblib
 
-random_forest = nfl_model.build_random_forest_model()
+X_train, X_test, y_train, y_test = nfl_model.gather_and_process_data("data/playbyplaydata/*")
+
+random_forest = nfl_model.build_random_forest_model(X_train,y_train)
 print(random_forest.predict([[4, 4, 1, 20, -3]]))
 
 joblib.dump(random_forest, 'Data/models/random_forest_classifier.pkl')
