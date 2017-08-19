f0c27baeab561fdaeac13d0bf0ff04dbe546c137
Michael Skrzypiec <skrzym@gmail.com>
2017-08-18 18:53:22 -0700

    App Updates

--- Application/Site/mmq/main/controllers.py
+++ Application/Site/mmq/main/controllers.py
@@ -1,11 +1,43 @@
 from flask import Blueprint, render_template, request, url_for
 from config import mongo
+import pandas as pd
 
+########################################################
+# Setup Pandas Masking Function
+# https://stackoverflow.com/questions/11869910/pandas-filter-rows-of-dataframe-with-operator-chaining
+def mask(df, key, value, operator='=='):
+    if type(value) is list:
+        if operator == 'not':
+            return df[df[key].apply(lambda x: x not in value)]
+        else:
+            return df[df[key].apply(lambda x: x in value)]
+    else:
+        if operator == '!=':
+            return df[df[key] != value]
+        elif operator == '>':
+            return df[df[key] > value]
+        elif operator == '>=':
+            return df[df[key] >= value]
+        elif operator == '<':
+            return df[df[key] < value]
+        elif operator == '<=':
+            return df[df[key] <= value]
+        else:
+            return df[df[key] == value]
+# Set Pandas DF Mask funciton
+pd.DataFrame.mask = mask
+
+# Pull Large DataFrame
+rs_pbp = pd.read_pickle('mmq\main\data\playbyplaydata\\rs_pbp.pkl')
+po_pbp = pd.read_pickle('mmq\main\data\playbyplaydata\\po_pbp.pkl')
+
+########################################################
+# Flask App
 main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/')
 def index():
-    mongo.db.visits.insert_one({"foo":"bar"})
+    mongo.db.visits.insert_one({"no":"way"})
     visits = mongo.db.visits.find_one()
     return str(visits)
     #return render_template('index.html')
@@ -51,6 +83,31 @@ def run():
     mongo.db.scenarios.insert_one(scenario)
     scenarios = mongo.db.scenarios.find()
 
+    # Convert mongoDB data to pandas DF
+    scenarios_df = pd.DataFrame.from_records(scenarios)
+    masks = [
+        ['down', down],
+        ['quarter', quarter],
+        ['clock', clock],
+        ['yards', yards],
+        ['field', field],
+        ['score', score],
+        #['guess', guess]
+    ]
+
+    # Apply provided masks to the scenarios data
+    # Can also be done via mongoDB filtering vs pandas
+    for mask in masks:
+        scenarios_df = scenarios_df.mask(mask[0], mask[1])
+    # Reset the Index and group by the different possible guesses
+    scenarios_df = scenarios_df.reset_index(drop=True)
+    guesses = scenarios_df.groupby('guess').agg({'_id':len}).reset_index()
+    guesses.columns= ['Guess','Count']
+    guesses_html = guesses.to_html(classes='table')
+
+    # Pull NFL Stats from MongoDB
+    nflstats = guesses_html
+
     try:
         return render_template('results.html',
         down=down,
@@ -60,7 +117,21 @@ def run():
         field=field,
         score=score,
         guess=guess,
-        scenarios=scenarios
+        scenarios=guesses_html,
+        nflstats=nflstats
     )
     except:
         return "fail"
+
+@main.route('/stats/')
+def tables():
+    title = 'Test Table'
+    #rs_pbp = pd.read_pickle('mmq\main\data\playbyplaydata\\rs_pbp.pkl')
+    #po_pbp = pd.read_pickle('mmq\main\data\playbyplaydata\\po_pbp.pkl')
+
+    rs_pass_attempts = rs_pbp.groupby('posteam').agg({'PassAttempt':sum,'RushAttempt':sum})
+    po_pass_attempts = po_pbp.groupby('posteam').agg({'PassAttempt':sum,'RushAttempt':sum})
+
+    table = rs_pass_attempts.to_html(classes='table')
+
+    return render_template('stats.html', table=table, title=title)
